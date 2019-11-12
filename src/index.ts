fe3e9db7acdd0d6342614277990e851c6760b1fb
Konstantin Kudelko <konstantinkudelko@mail.ru>
2019-11-12 11:28:38 +0300

    add: main keyboard

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
 require('dotenv').config();
 import Telegraf from 'telegraf';
 import { connect, connection } from 'mongoose';
-import { logger } from './utils/logger.util';
 
-connect(
-  process.env.DB_CONNECTION_STRING,
-  { useNewUrlParser: true, useUnifiedTopology: true },
-)
+import { logger } from './utils/logger.util';
+import { getMainKeyboard } from './utils/keyboard.util';
+
+connect(process.env.DB_CONNECTION_STRING, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
   .then(() => logger.info('Successfull database connection'))
   .catch(err =>
     logger.error(
@@ -17,8 +19,9 @@ connect(
 
 connection.on('open', () => {
   const bot = new Telegraf(process.env.BOT_TOKEN);
+  const { mainKeyboard } = getMainKeyboard();
 
-  bot.start(ctx => ctx.reply('Welcome'));
+  bot.start(ctx => ctx.reply('Welcome', mainKeyboard));
 
   bot.launch();
 });
