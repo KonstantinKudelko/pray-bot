7b017193803c2f6564f3590ecbaacb33c2c4aef2
Konstantin Kudelko <konstantinkudelko@mail.ru>
2020-01-16 16:12:45 +0300

    add (renind): decription for random needs list

--- src/scenes/remind/lib/random-needs-list/index.ts
+++ src/scenes/remind/lib/random-needs-list/index.ts
@@ -1,12 +1,15 @@
 import { Extra, Markup } from 'telegraf';
 
 import { Need } from '../../../../models';
+import { decrypt } from '../../../../scenes/lib/encrypt';
 
 export const randomNeedsList = (needs: Need[], prayedButtonText: string) => {
   return Extra.HTML().markup((m: Markup) => {
     return m.inlineKeyboard(
       [
-        ...needs.map(x => [m.callbackButton(x.name, JSON.stringify(null), false)]),
+        ...needs.map(x => [
+          m.callbackButton(decrypt(x.name), JSON.stringify(null), false),
+        ]),
         [
           {
             text: prayedButtonText,
